662b0e7325e3c97a4bf34bba564fe64cce13b7d9
Josenei Santana <joseneysantana@yahoo.com>
2023-10-14 09:49:02 -0300

    Add logica ao projeto

---
+++ main.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form-atividade');
+form.addEventListener('submit', function(e){
+     e.preventDefault();
+
+     const inputNomeAtividade = document.getElementById('nome-atividade');
+     const inputNotaAtividade  =document.getElementById('nota-atividade');
+})
