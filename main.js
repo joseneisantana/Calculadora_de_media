f696dd819ce606fe15b956f9a6b520669b75283e
Josenei Santana <joseneysantana@yahoo.com>
2023-10-14 11:12:58 -0300

    Finalizada primeira parte do JS

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 const form = document.getElementById('form-atividade');
+let linhas = '';
 form.addEventListener('submit', function(e){
      e.preventDefault();
 
      const inputNomeAtividade = document.getElementById('nome-atividade');
      const inputNotaAtividade  =document.getElementById('nota-atividade');
-})
+     const imgAprovado ='<img src ="./images/aprovado.png" alt="iEmoji celebrando"/>'
+     const imgReprovado ='<img src ="./images/reprovado.png" alt="iEmoji decepcionado"/>'
+    
+
+  let linha = '<tr>';
+  linha += `<td> ${inputNomeAtividade.value}</td>`;
+  linha += `<td> ${inputNotaAtividade.value}</td>`;
+  linha += `<td> ${inputNotaAtividade.value >= 7 ?  imgAprovado : imgReprovado}</td>`;
+  linha += '</tr>';
+
+linhas += linha;
+
+const corpoTabela = document.querySelector('tbody');
+ corpoTabela.innerHTML =linhas;
+
+ inputNomeAtividade.value ='';
+ inputNomeAtividade.value ='';
+     
+});
